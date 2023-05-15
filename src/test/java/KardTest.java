ed923a9228566fedc3d5c7d08d6bcf75ef65a20d
Maria Loginova <marialog@yandex.ru>
2023-05-15 14:32:04 +0300

    test not found agan

--- src/test/java/KardTest.java
+++ src/test/java/KardTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -6,10 +7,12 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class KardTest {
     SelenideElement form = $(".form");
-    @Test
+
+    @BeforeEach
     public void setup() {
         open("http://localhost:7777/");
     }
+    @Test
     public void shouldTest1() {
         form.$("[data-test-id=name] input").setValue("Логинова Мария");
         form.$("[data-test-id=phone] input").setValue("+79219864218");
